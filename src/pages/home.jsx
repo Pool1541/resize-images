3cef3017ced00f5d2557435c5eb3bc799c5d935a
pool_1541 <pool_1541@hotmail.com>
2024-04-27 21:44:15 -0500

    feat(page): add home page

---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import { Form } from "@/components/form";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center py-10 h-dvh">
+      <Form />
+    </main>
+  );
+}
