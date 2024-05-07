5042f9ac8394546deea099671821ea695161da38
Pool Llerena <pool_1541@hotmail.com>
2024-05-07 12:47:51 -0500

    chore(vite): update base url

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  base: 'https://pool_1541.github.io/resize-images',
+  base: '/resize-images/',
   build: {
     outDir: 'build'
   }
