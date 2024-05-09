fc9218b838d5619535543d30a798b37ffb00bf95
Pool Llerena <pool_1541@hotmail.com>
2024-05-09 18:12:30 -0500

    feat(component); add rename component

--- src/pages/resize.jsx
+++ src/pages/resize.jsx
@@ -1,14 +1,26 @@
-import ResizeImageForm from '@/components/resize-image-form';
-import useImages from '@/hooks/useImages';
-import useResize from '@/hooks/useResize';
-import { Button } from '@material-tailwind/react';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import Rename from "@/components/rename";
+import ResizeImageForm from "@/components/resize-image-form";
+import useImages from "@/hooks/useImages";
+import useLabels from "@/hooks/useLabels";
+import useResize from "@/hooks/useResize";
+import { Button } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Resize() {
+  const [label, setLabel] = useState('');
+  const {saveLabel} = useLabels();
   const { images } = useImages();
   const resize = useResize();
   const navigate = useNavigate();
+
+  function handleChange(e) {
+    if (!e.target) {
+      setLabel(e)
+    } else {
+      setLabel(e.target.value)
+    }
+  }
 
   async function download() {
     const resizedImages = await Promise.all(
@@ -18,28 +30,32 @@ export default function Resize() {
     );
 
     resize.download(resizedImages);
+    saveLabel(label);
   }
 
   useEffect(() => {
     if (!images) {
-      return navigate('/');
+      return navigate("/");
     }
   }, [navigate, images]);
 
   const arrayOfImages = Array.from(images || []);
 
   return (
-    <section className='min-h-dvh max-w-7xl flex items-center gap-4 m-auto'>
-      <div className='grid grid-cols-3 w-[768px] gap-y-4'>
+    <section className="min-h-dvh max-w-7xl flex items-center gap-4 m-auto">
+      <div className="grid grid-cols-3 w-[768px] gap-y-4">
         {arrayOfImages.map((image, i) => (
           <ResizeImageForm imageFile={image} key={i} />
         ))}
       </div>
-      <div className='flex items-center justify-center flex-1'>
-        <Button className='w-1/2 bg-blue-gray-800' onClick={download}>
+
+      <div className="flex flex-col items-center justify-center flex-1">
+        <Rename label={label} changeLabel={handleChange}/>
+        <Button className="w-1/2 bg-blue-gray-800" onClick={download}>
           Descargar
         </Button>
       </div>
+
     </section>
   );
 }
