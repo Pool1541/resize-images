a94418a04309bfe25fb14ad9cefcbda26e53569b
Pool Llerena <pool_1541@hotmail.com>
2024-05-09 18:11:39 -0500

    feat(context): add labelsContext

--- src/router.jsx
+++ src/router.jsx
@@ -2,15 +2,18 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "@/pages/home";
 import Resize from "@/pages/resize";
 import ImageContextProvider from "./contexts/images-context";
+import LabelsContextProvider from "./contexts/labels-context";
 
 export default function AppRouter() {
   return (
-    <Router>
+    <Router basename="/resize-images/">
       <ImageContextProvider>
+        <LabelsContextProvider>
         <Routes>
           <Route path="*" element={<Home />} />
           <Route path="/resize" element={<Resize />} />
         </Routes>
+        </LabelsContextProvider>
       </ImageContextProvider>
     </Router>
   );
