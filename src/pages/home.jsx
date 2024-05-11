e32dc84a5e8a5faae2cea0066bdb4fb385d9f4cc
pool_1541 <pool_1541@hotmail.com>
2024-05-11 12:43:08 -0500

    feat(seo): add react helmet to improve seo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,15 @@
-import { Form } from "@/components/form";
+import { Form } from '@/components/form';
+import { Helmet } from 'react-helmet-async';
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center py-10 h-dvh">
-      <Form />
-    </main>
+    <>
+      <Helmet>
+        <title>Resize images</title>
+      </Helmet>
+      <main className='flex items-center justify-center py-10 h-dvh'>
+        <Form />
+      </main>
+    </>
   );
 }
--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,19 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "@/pages/home";
-import Resize from "@/pages/resize";
-import ImageContextProvider from "./contexts/images-context";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from '@/pages/home';
+import Resize from '@/pages/resize';
+import ImageContextProvider from './contexts/images-context';
+import { HelmetProvider } from 'react-helmet-async';
 
 export default function AppRouter() {
   return (
     <Router>
       <ImageContextProvider>
-        <Routes>
-          <Route path="*" element={<Home />} />
-          <Route path="/resize" element={<Resize />} />
-        </Routes>
+        <HelmetProvider>
+          <Routes>
+            <Route path='*' element={<Home />} />
+            <Route path='/resize' element={<Resize />} />
+          </Routes>
+        </HelmetProvider>
       </ImageContextProvider>
     </Router>
   );
